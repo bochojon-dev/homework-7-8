0b0cfcec88439aba33a70c471c730bc131b5f860
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-05-23 10:07:51 +0500

    cards

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,7 +3,7 @@ import "./Products.scss";
 import { Link } from "react-router-dom";
 
 const Products = ({ loading, data }) => {
-  let productItems = data?.slice(2)?.map((el) => (
+  let productItems = data?.slice(3)?.map((el) => (
     <div key={el.id} className="products__card">
       <Link to={"/about"}>
         <img className="image0" src={el.images[0]} alt="product" />
@@ -11,6 +11,10 @@ const Products = ({ loading, data }) => {
       <Link to={"/about"}>
         <img className="image1" src={el.images[1]} alt="product" />
       </Link>
+      <p title={el.description} className="description">
+        {el.description}{" "}
+      </p>
+      <p>{el.stock} ta xarid</p>
       <h3>{el.title}</h3>
     </div>
   ));
